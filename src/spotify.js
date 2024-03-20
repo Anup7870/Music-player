f1f0a64c96e32e39dc8bb9e85c7c47e9dcedecdd
Anup7870 <kumaranup2505@gmail.com>
2024-03-20 10:36:59 +0530

    final

--- src/spotify.js
+++ src/spotify.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 
-const authEndpoint = "https://accounts.spotify.com/authorize?";
-const clientId = "10ff71b2181a4ee19909431870ef03f4";
+// const authEndpoint = "https://accounts.spotify.com/authorize?";
+// const clientId = "10ff71b2181a4ee19909431870ef03f4";
 const redirectUri = "http://localhost:3000";
 const scopes = ["user-library-read", "playlist-read-private"];
-
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const clientId = "1fab860e3d7c4f3ebea0c715b5e35a39";
+// const redirectUri='https://music-player-chi-three.vercel.app'
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-  "%20"
+   "%20"
 )}&response_type=token&show_dialog=true`;
 
 const apiClient = axios.create({
-  baseURL: "https://api.spotify.com/v1/",
+   baseURL: "https://api.spotify.com/v1/",
 });
 
 export const setClientToken = (token) => {
-  apiClient.interceptors.request.use(async function (config) {
-    config.headers.Authorization = "Bearer " + token;
-    return config;
-  });
+   apiClient.interceptors.request.use(async function (config) {
+      config.headers.Authorization = "Bearer " + token;
+      return config;
+   });
 };
 
 export default apiClient;
